357d1b51cb3e6a9535097d433c9daba55acdc7b6
sinan <aslansinan586@gmail.com>
2022-12-25 01:26:09 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Components/Headers'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <p className='asd'>dfgfpdcvf pcmv dcfc dfvp</p>
+      <label htmlFor='deneme'>Name</label>
+      <input id='deneme' />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Headers.js
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <div>
+            Merhaba React   
+        </div>
+    )
+}
+export default Header;
