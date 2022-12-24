ace3079b52251b68a50fdecc7402d7bfa71f9796
sinan <aslansinan586@gmail.com>
2022-12-25 01:32:06 +0300

    değişken render etme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import Header from './Components/Headers'
-
+const name =  "Sinan";
+const surname =  "Aslan";
 function App() {
   return (
-    <div>
-      <Header />
-      <p className='asd'>dfgfpdcvf pcmv dcfc dfvp</p>
-      <label htmlFor='deneme'>Name</label>
-      <input id='deneme' />
-    </div>
+    <>
+    <h1>{`Benim adım ${name}, soyadım ${surname}`} </h1>
+    </>
   );
 }
 
